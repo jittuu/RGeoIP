5e135530ade1c55c135ec733ea212dfce1f6dcbf
Soe Moe <soemoetunlwin@gmail.com>
2014-11-17 00:52:03 +0800

    initial commit

---
+++ RGeoIP/IPCountry.cs
@@ -0,0 +1,16 @@
+﻿
+namespace RGeoIP {
+    public class IPCountry {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string MaxIPAddress { get; set; }
+
+        public double MaxIPNumber { get; set; }
+
+        public string MinIPAddress { get; set; }
+
+        public double MinIPNumber { get; set; }
+    }
+}
